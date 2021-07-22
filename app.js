80a60819a38c3f070d379c8b1620aee5fb16c59d
Shreenath Ranganathan <shreenath.ranganathan@gmail.com>
2021-07-22 10:47:53 +0530

    Handling CORS
    Handling CORS

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const orderRoutes = require('./api/routes/order-route');
 
 //logger
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));//alternative to body-parser
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+//Setting Headers to response
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods',"GET, POST, PATCH, DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 //use routes
 app.use('/product',productRoutes);
